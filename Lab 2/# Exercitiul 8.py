3704d10daa203277629b5ed3483b589c5218cc07
sebimatei2511 <79249972+sebimatei2511@users.noreply.github.com>
2022-10-20 17:10:43 +0300

    Update # Exercitiul 8.py

--- Lab 2/# Exercitiul 8.py
+++ Lab 2/# Exercitiul 8.py
@@ -5,4 +5,25 @@
 # Example: x = 2, ["test", "hello", "lab002"], flag = False 
 # will return (["e", "s"], ["e" . Note: The function must return list of lists.
 
-# PROBLEMS
+def the_function(list_of_string, x = 1, flag = True):
+    result_list = []
+
+    for string_input in list_of_string:
+        new_list = []
+
+        if flag == True:
+            for index in string_input:
+                if ord(index) % x == 0:
+                    new_list.append(index)
+
+        elif flag == False:
+            for index in string_input:
+                if ord(index) % x != 0:
+                    new_list.append(index)
+
+        result_list.append(new_list)
+    
+    return result_list
+
+print(the_function(["test", "hello", "lab002"], 2, False))
+print(the_function(["test", "hello", "lab002"]))
